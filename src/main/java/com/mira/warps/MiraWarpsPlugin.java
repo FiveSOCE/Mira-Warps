edf050a05f6f3da080823ffb29fa96e7daffc015
FiveS <sgmmsionide@gmail.com>
2026-09-03 06:42:37 +1000

    Add MiraWarps plugin bootstrap

---
+++ src/main/java/com/mira/warps/MiraWarpsPlugin.java
@@ -0,0 +1,44 @@
+package com.mira.warps;
+
+import com.mira.warps.gui.WarpGuiListener;
+import com.mira.warps.gui.WarpGuiService;
+import com.mira.warps.listener.WarpCommandBridgeListener;
+import com.mira.warps.particle.WarpParticleService;
+import net.ess3.api.IEssentials;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MiraWarpsPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        Plugin essentialsPlugin = getServer().getPluginManager().getPlugin("Essentials");
+        if (!(essentialsPlugin instanceof IEssentials essentials)) {
+            getLogger().severe("EssentialsX 2.22.0+ is required. Disabling MiraWarps.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        WarpGuiService gui = new WarpGuiService(this, essentials);
+        WarpParticleService particles = new WarpParticleService(this);
+
+        getServer().getPluginManager().registerEvents(new WarpGuiListener(gui, particles), this);
+        getServer().getPluginManager().registerEvents(new WarpCommandBridgeListener(gui), this);
+
+        PluginCommand command = getCommand("mwarps");
+        if (command != null) {
+            command.setExecutor((sender, cmd, label, args) -> {
+                if (!(sender instanceof org.bukkit.entity.Player player)) {
+                    sender.sendMessage("MiraWarps can only be opened by a player.");
+                    return true;
+                }
+                gui.open(player);
+                return true;
+            });
+        }
+
+        getLogger().info("MiraWarps enabled with " + gui.warpNames().size() + " Essentials warp(s).");
+    }
+}
