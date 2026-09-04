87589388f2be9e12d05851124fc8e7629e5437ca
FiveS <sgmmsionide@gmail.com>
2026-09-04 20:05:09 +1000

    Delegate warp teleport visuals to MiraCosmetics

--- src/main/java/com/mira/warps/MiraWarpsPlugin.java
+++ src/main/java/com/mira/warps/MiraWarpsPlugin.java
@@ -3,7 +3,6 @@ package com.mira.warps;
 import com.mira.warps.gui.WarpGuiListener;
 import com.mira.warps.gui.WarpGuiService;
 import com.mira.warps.listener.WarpCommandBridgeListener;
-import com.mira.warps.particle.WarpParticleService;
 import net.ess3.api.IEssentials;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.command.PluginCommand;
@@ -11,7 +10,8 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MiraWarpsPlugin extends JavaPlugin {
-    private static final LegacyComponentSerializer LEGACY = LegacyComponentSerializer.legacyAmpersand();
+    private static final LegacyComponentSerializer LEGACY =
+            LegacyComponentSerializer.legacyAmpersand();
     private static final String CHAT_PREFIX = "&5&lMira &8>> &r";
 
     @Override
@@ -26,16 +26,16 @@ public final class MiraWarpsPlugin extends JavaPlugin {
         }
 
         WarpGuiService gui = new WarpGuiService(this, essentials);
-        WarpParticleService particles = new WarpParticleService(this);
 
-        getServer().getPluginManager().registerEvents(new WarpGuiListener(gui, particles), this);
+        getServer().getPluginManager().registerEvents(new WarpGuiListener(gui), this);
         getServer().getPluginManager().registerEvents(new WarpCommandBridgeListener(gui), this);
 
         PluginCommand command = getCommand("mwarps");
         if (command != null) {
             command.setExecutor((sender, cmd, label, args) -> {
                 if (!(sender instanceof org.bukkit.entity.Player player)) {
-                    sender.sendMessage(LEGACY.deserialize(CHAT_PREFIX + "&cMiraWarps can only be opened by a player."));
+                    sender.sendMessage(LEGACY.deserialize(
+                            CHAT_PREFIX + "&cMiraWarps can only be opened by a player."));
                     return true;
                 }
                 gui.open(player);
@@ -43,6 +43,8 @@ public final class MiraWarpsPlugin extends JavaPlugin {
             });
         }
 
-        getLogger().info("MiraWarps enabled with " + gui.warpNames().size() + " Essentials warp(s).");
+        getLogger().info("MiraWarps v" + getPluginMeta().getVersion()
+                + " enabled with " + gui.warpNames().size()
+                + " Essentials warp(s). Successful teleports are visualized by MiraCosmetics when installed.");
     }
 }
